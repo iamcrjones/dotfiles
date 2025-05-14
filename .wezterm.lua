a1b6b446d9b5824f3b006cc2adbc4d496e3cd4b3
Cameron Jones <iamcrjones@gmail.com>
2025-05-14 22:45:10 +1000

    init

---
+++ .wezterm.lua
@@ -0,0 +1,26 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+--Fonts
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 18
+
+--Make sure mac alt key nav with zellij working
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
+config.use_ime = false
+
+--Remove tab bar
+config.enable_tab_bar = false
+
+--Colors
+config.color_scheme = "Tokyo Night"
+
+--Window config
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.925
+config.macos_window_background_blur = 15
+
+config.default_prog = { "/opt/homebrew/bin/tmux" }
+
+return config
