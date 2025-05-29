03e3e13d3927eddc22647c02eb0c0eb64ed78ea7
Cameron Jones <iamcrjones@gmail.com>
2025-05-30 09:32:31 +1000

    neovim update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,25 +1,34 @@
 return {
-  {
-    "mason-org/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
-  }
-,{
-    "mason-org/mason-lspconfig.nvim",
-    opts = {ensure_installed = {"lua_ls","ts_ls"}},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-    },
-},{
-        "neovim/nvim-lspconfig",
-    config = function()
-    local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "lua_ls", "ts_ls", "html", "intelephense", "marksman", "pest_ls", "tailwindcss", "jsonls" },
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.ts_ls.setup({})
+			lspconfig.html.setup({})
+			lspconfig.intelephense.setup({})
+			lspconfig.marksman.setup({})
+			lspconfig.pest_ls.setup({})
+			lspconfig.tailwindcss.setup({})
+			lspconfig.jsonls.setup({})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,16 @@
-return {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate",
-config = function () 
-      local configs = require("nvim-treesitter.configs")
+return {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html","php","blade" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
+		configs.setup({
+			ensure_installed = { "lua", "vim", "vimdoc", "javascript", "html", "php", "blade" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
