4656f5bf10d207bd901396bcdc19522064332409
Cameron Jones <iamcrjones@gmail.com>
2025-05-29 16:52:30 +1000

    update nvim config with which-key and lazygit

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -7,7 +7,7 @@ return {
   }
 ,{
     "mason-org/mason-lspconfig.nvim",
-    opts = {ensure_installed = {"lua_ls","tsserver"}},
+    opts = {ensure_installed = {"lua_ls","ts_ls"}},
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
     },
@@ -16,7 +16,7 @@ return {
     config = function()
     local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+      lspconfig.ts_ls.setup({})
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
---
+++ nvim/lua/plugins/which-key.lua
@@ -0,0 +1,27 @@
+return {
+    'folke/which-key.nvim',
+    event = 'VeryLazy', -- Load very late to avoid startup impact
+    config = function()
+      require('which-key').setup({
+        -- Configure which-key options here if needed
+        -- For example, change the popup delay:
+        -- delay = 100,
+      })
+    local wk = require("which-key")
+wk.add({
+  { "<leader>e", "<cmd>Oil<cr>", desc = "Open Oil", mode = "n" }, 
+  { "<leader>f", group = "file" }, -- group
+  { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
+  { "<leader>g", group = "git" }, -- group
+  { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit", mode = "n" }, 
+  {
+    -- Nested mappings are allowed and can be added in any order
+    -- Most attributes can be inherited or overridden on any level
+    -- There's no limit to the depth of nesting
+    mode = { "n", "v" }, -- NORMAL and VISUAL mode
+    { "<leader>q", "<cmd>q<cr>", desc = "Quit" }, -- no need to specify mode since it's inherited
+    { "<leader>w", "<cmd>w<cr>", desc = "Write" },
+  }
+})
+    end,
+  }
