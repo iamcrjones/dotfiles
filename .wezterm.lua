bbd8c628d022ec97978a277d6d941af098370087
Cameron Jones <iamcrjones@gmail.com>
2025-06-08 19:34:30 +1000

    add everforest wallpaper, switch to everforest theme wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,10 +16,12 @@ config.enable_tab_bar = false
 --Colors
 -- config.color_scheme = "Tokyo Night"
 -- config.color_scheme = "Palenight (Gogh)"
-config.color_scheme = "Kanagawa (Gogh)"
+-- config.color_scheme = "Kanagawa (Gogh)"
+config.color_scheme = "Everforest Dark (Gogh)"
+
 --Window config
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.75
 config.macos_window_background_blur = 15
 
 -- config.default_prog = { "/opt/homebrew/bin/tmux" }
